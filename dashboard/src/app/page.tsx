6dc6e10e1aaed7f0ccb6781ac2fd7ed24c3b3aa8
naimulh247 <naimulh247@gmail.com>
2025-03-13 16:01:53 -0400

    redirect to dashboard for now

--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -1,12 +1,16 @@
 import Dashboard from "@/components/common/dashboard";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  // redirect to dashboard
+  redirect("/dashboard");
   return (
     // <Dashboard/>
     // login in or redirect to dashboard
     <div>
       <p>Login or redirect to dashboard</p>
     </div>
+
   );
 }
